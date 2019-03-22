dfcf23dcac17d997e0e44aa84769f58c9085b9a1
raisantos <raisoledade@outlook.com>
2019-03-22 14:18:23 -0400

    model

--- src/main/java/com/loopa/api/model/Avaliacao.java
+++ src/main/java/com/loopa/api/model/Avaliacao.java
@@ -1,5 +1,63 @@
 package com.loopa.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Avaliacao {
 
+	@Id
+	@GeneratedValue
+	private int id;
+	private String comentario;
+	private int nota;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	public int getNota() {
+		return nota;
+	}
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comentario == null) ? 0 : comentario.hashCode());
+		result = prime * result + id;
+		result = prime * result + nota;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Avaliacao other = (Avaliacao) obj;
+		if (comentario == null) {
+			if (other.comentario != null)
+				return false;
+		} else if (!comentario.equals(other.comentario))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nota != other.nota)
+			return false;
+		return true;
+	}
 }
--- src/main/java/com/loopa/api/model/Cliente.java
+++ src/main/java/com/loopa/api/model/Cliente.java
@@ -6,6 +6,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Cliente {
+	
 	@Id
 	@GeneratedValue
 	private Long id;
@@ -40,5 +41,41 @@ public class Cliente {
 	public void setPassportNumber(String passportNumber) {
 		this.passportNumber = passportNumber;
 	}
-		
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((passportNumber == null) ? 0 : passportNumber.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (passportNumber == null) {
+			if (other.passportNumber != null)
+				return false;
+		} else if (!passportNumber.equals(other.passportNumber))
+			return false;
+		return true;
+	}
 }
