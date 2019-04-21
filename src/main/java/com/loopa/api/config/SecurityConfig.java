c2a025df3eb740848cc5d4ab6e7153d258c518eb
raisantos <raisoledade@outlook.com>
2019-04-21 14:07:57 -0400

    implementacao autorizaçao

--- src/main/java/com/loopa/api/config/SecurityConfig.java
+++ src/main/java/com/loopa/api/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.loopa.api.security.JWTUtil;
+import com.loopa.api.security.JWTAuthorizationFilter;
 import com.loopa.api.security.JWTAuthenticationFilter;
 
 @Configuration
@@ -64,6 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
